e01923691ea6ba2366fd4a5758fdc986e9bb6bcc
Bryan Maciel <bryanmacieldealmeida06@gmail.com>
2025-04-12 00:37:03 -0300

    Ajustes técnicos e organização

--- script/script.js
+++ script/script.js
@@ -8,8 +8,6 @@ const randomNumber = Math.floor(Math.random() * 4);
 console.log(`imagem inicial: ${randomNumber + 1}`);
 let index = randomNumber;
 const slider = document.getElementById('slider');
-
-// primeira imagem
 let atual = document.createElement('div');
 atual.classList.add('slide');
 atual.style.backgroundImage = `url(${imagens[index]})`;
@@ -22,32 +20,10 @@ setInterval(() => {
     nova.classList.add('slide');
     nova.style.backgroundImage = `url(${imagens[index]})`;
     slider.appendChild(nova);
-
-    // remove a anterior depois da animação
     setTimeout(() => {
         const slides = document.querySelectorAll('.slide');
         if (slides.length > 1) {
             slides[0].remove();
         }
-    }, 1000); // igual ao tempo da animação
+    }, 1000);
 }, 4000);
-function darkMode() {
-    let mode = "ninja";
-    let color = "ghostWhite";
-    let image = "/img/light-icon.png";
-    let label = "Light Mode";
-    if (mode === "dark") {
-        color = "DarkSlateBlue";
-        image = "/img/dark-icon.png";
-        label = "Dark Mode";
-    } else if (mode === "light") {
-        mode = "light";
-        color = "ghostWhite";
-        image = "/img/light-icon.png";
-        label = "Light Mode";
-    } else {
-        color = "dimGray";
-        image = "/img/ninja-icon.png";
-        label = "Ninja Mode";
-    }
-}
